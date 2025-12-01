85e12adb948a66d493f1ba62a14cc92377cfffbf
Oscar Barlow <oscar.barlow@starlingbank.com>
2025-12-01 12:10:53 +0000

    test the cli

--- lib/todo/adapters/cli.ex
+++ lib/todo/adapters/cli.ex
@@ -1,53 +1,49 @@
 defmodule Todo.Adapters.Cli do
   alias Todo.Core.Task
   alias Todo.Core.TaskList
-  
+
   @task_list_module Application.compile_env(:elixir_todo, :task_list_module, Todo.Core.TaskList)
-  
+  @cli_formatter_module Application.compile_env(:elixir_todo, :cli_formatter_module, Todo.Adapters.CliFormatter)
+
   @behaviour Todo.Ports.Cli
-  
+
   # this module should actually format the tasks, so that we're always returning a string
   # then orchestration code can figure just take input and print output
   # my storage adapter should be responsible for saving and formatting files to disk, and reading files into tasklists
   # possibly the formatter needs to live in its own module.
-	
+
   @impl true
   def parse(%TaskList{} = task_list, command) do
     String.split(command)
     |> then(fn cmd -> run(task_list, cmd) end)
   end
-  
+
   defp run(%TaskList{} = task_list, ["add" | description_list]) do
     task = Enum.join(description_list, " ")
       |> then(fn description -> %Task{description: description} end)
-    added = @task_list_module.add_task_to_list(task_list, task)
-    IO.puts("Added task")
-    added
+    @task_list_module.add_task_to_list(task_list, task)
+    {:ok, "Added task"}
   end
-  
+
   defp run(%TaskList{} = task_list, ["done", index]) do
-    done = @task_list_module.mark_task_as_done(task_list, String.to_integer(index))
-    IO.puts("Marked task as done")
-    done
+    @task_list_module.mark_task_as_done(task_list, String.to_integer(index))
+    {:ok, "Marked task #{index} as done"}
   end
-  
+
   defp run(%TaskList{} = task_list, ["list"]) do
-    task_list.tasks
-    |> Enum.each(&IO.puts(&1.description))
-    :ok
+    formatted = @cli_formatter_module.format(task_list)
+    {:ok, formatted}
   end
-  
+
   defp run(%TaskList{} = task_list, ["list", "--not-done"]) do
-    @task_list_module.get_not_done_tasks(task_list)
-    |> Map.get(:tasks)
-    |> Enum.each(&IO.puts(&1.description))
-    :ok
+    not_done_tasks = @task_list_module.get_not_done_tasks(task_list)
+    formatted = @cli_formatter_module.format(not_done_tasks)
+    {:ok, formatted}
   end
-  
+
   defp run(%TaskList{} = task_list, ["remove", index]) do
-    removed = @task_list_module.remove_task_from_list(task_list, String.to_integer(index))
-    IO.puts("Removed task from list")
-    removed
+    @task_list_module.remove_task_from_list(task_list, String.to_integer(index))
+    {:ok, "Removed task #{index}"}
   end
-  
+
 end
--- test/todo/adapters/cli_test.exs
+++ test/todo/adapters/cli_test.exs
@@ -1,11 +1,12 @@
 defmodule Todo.Adapters.CliTest do
   use ExUnit.Case
   import Mox
-  
+
+  alias Todo.Adapters.CliFormatter
   alias Todo.Core.TaskList
   alias Todo.Core.Task
   alias Todo.Adapters.Cli
-  
+
   setup do
     :verify_on_exit!
     :ok
@@ -14,94 +15,117 @@ defmodule Todo.Adapters.CliTest do
   describe "when adding tasks" do
     test "should delegate adding tasks" do
       task_list = %TaskList{tasks: []}
-      
-      expect(TaskListMock, :add_task_to_list, fn ^task_list, %Task{description: "buy some milk"} ->
+
+      expect(TaskListMock, :add_task_to_list, fn ^task_list,
+                                                 %Task{description: "buy some milk"} ->
         %TaskList{tasks: [%Task{description: "buy some milk"}]}
       end)
-      
+
       Cli.parse(%TaskList{tasks: []}, "add buy some milk")
     end
 
-    test "should return task list with task added" do
+    test "should tell you that you've added a task" do
       task_list = %TaskList{tasks: []}
-      
-      expect(TaskListMock, :add_task_to_list, fn ^task_list, %Task{description: "buy some milk"} ->
+
+      expect(TaskListMock, :add_task_to_list, fn ^task_list,
+                                                 %Task{description: "buy some milk"} ->
         %TaskList{tasks: [%Task{description: "buy some milk"}]}
       end)
-      
-      result = Cli.parse(%TaskList{tasks: []}, "add buy some milk")
-      expected = """
-      1. buy some milk
-      """
-      
-      assert result == expected
+
+      added = Cli.parse(%TaskList{tasks: []}, "add buy some milk")
+      assert added == {:ok, "Added task"}
     end
+  end
+
+  describe "when marking tasks as done" do
+    test "should delegate marking a task as done" do
+      shopping = %Task{description: "do the shopping"}
+      walk_dog = %Task{description: "walk the dog", is_done: true}
+      dinner = %Task{description: "cook dinner"}
+
+      task_list = %TaskList{tasks: [shopping, walk_dog, dinner]}
+      _any_task_list = %TaskList{tasks: []}
+
+      expect(TaskListMock, :mark_task_as_done, fn ^task_list, 3 -> _any_task_list end)
+
+      result = Cli.parse(task_list, "done 3")
+      assert result == {:ok, "Marked task 3 as done"}
+    end
+
   end
 
   describe "when listing tasks" do
-    test "should list all tasks" do
+    test "should delegate formatting to the formatter for listing all tasks" do
       shopping = %Task{description: "do the shopping"}
       walk_dog = %Task{description: "walk the dog", is_done: true}
       dinner = %Task{description: "cook dinner"}
-      
+
       task_list = %TaskList{tasks: [shopping, walk_dog, dinner]}
-      
+
+      expect(CliFormatterMock, :format, fn ^task_list -> "formatted output" end)
+
       result = Cli.parse(task_list, "list")
-      expected = """
-      1. do the shopping
-      2. walk the dog ✓
-      3. cook dinner
-      """
-      
-      assert result == expected
+      assert result == {:ok, "formatted output"}
     end
 
-    test "should list only not-done tasks, given flag is passed" do
+    test "should delegate filtering to only not-done tasks, given flag is passed" do
       shopping = %Task{description: "do the shopping"}
       walk_dog = %Task{description: "walk the dog", is_done: true}
       dinner = %Task{description: "cook dinner"}
-      
+
       task_list = %TaskList{tasks: [shopping, walk_dog, dinner]}
-      
-      expect(TaskListMock, :get_not_done_tasks, fn ^task_list -> %TaskList{tasks: [shopping, dinner]} end)
-      
+      not_done_task_list = %TaskList{tasks: [shopping, dinner]}
+
+      expect(TaskListMock, :get_not_done_tasks, fn ^task_list ->
+        not_done_task_list
+      end)
+    end
+
+    test "should delegate formatting to the formatter for listing not-done tasks" do
+      shopping = %Task{description: "do the shopping"}
+      walk_dog = %Task{description: "walk the dog", is_done: true}
+      dinner = %Task{description: "cook dinner"}
+
+      task_list = %TaskList{tasks: [shopping, walk_dog, dinner]}
+      not_done_task_list = %TaskList{tasks: [shopping, dinner]}
+
+      expect(TaskListMock, :get_not_done_tasks, fn ^task_list ->
+        not_done_task_list
+      end)
+
+      expect(CliFormatterMock, :format, fn ^not_done_task_list -> "formatted output" end)
+
       result = Cli.parse(task_list, "list --not-done")
-      expected = """
-      1. do the shopping
-      2. cook dinner
-      """
-      
-      assert result == expected
+      assert result == {:ok, "formatted output"}
     end
   end
-  
+
   describe "when removing tasks" do
     test "should delegate removing tasks" do
       shopping = %Task{description: "do the shopping"}
       walk_dog = %Task{description: "walk the dog", is_done: true}
-      
+
       task_list = %TaskList{tasks: [shopping, walk_dog]}
-      
-      expect(TaskListMock, :remove_task_from_list, fn ^task_list, 2 -> %TaskList{tasks: [shopping]} end)
-    
+
+      expect(TaskListMock, :remove_task_from_list, fn ^task_list, 2 ->
+        %TaskList{tasks: [shopping]}
+      end)
+
       Cli.parse(task_list, "remove 2")
     end
-    
-    test "should return task list without removed task" do
+
+    test "should tell you that you've removed a task" do
       shopping = %Task{description: "do the shopping"}
       walk_dog = %Task{description: "walk the dog", is_done: true}
-      
+
       task_list = %TaskList{tasks: [shopping, walk_dog]}
-      
-      expect(TaskListMock, :remove_task_from_list, fn ^task_list, 2 -> %TaskList{tasks: [shopping]} end)
-    
+
+      expect(TaskListMock, :remove_task_from_list, fn ^task_list, 2 ->
+        %TaskList{tasks: [shopping]}
+      end)
+
       result = Cli.parse(task_list, "remove 2")
-      
-      expected = """
-      1. do the shopping
-      """
-      
-      assert result == expected
+      assert result == {:ok, "Removed task 2"}
     end
   end
 end
